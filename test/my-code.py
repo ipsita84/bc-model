9c60e964fdfe04c1491585cc462bc40d6e6e8de5
Atri Bhattacharya <badshah400@gmail.com>
2015-07-21 21:55:17 -0700

    Fix use of numpy.append.

--- test/my-code.py
+++ test/my-code.py
@@ -20,18 +20,19 @@ def main():
         if b not in betas:
             betas.append(b)
         fdict[(s,b)] = i
+    
     for b in betas:
         T=1./float(b)
         for s in sizes:
             d = np.loadtxt(fdict[(s,b)])
             r=int(s/2)
             for i in range(1,r):
-                resarr.append(s, i*1./s, d[:,0], d[:,1])
+                resarr.append([s, i*1./s, d[-1,0], d[-1,1]])
 
     res = np.reshape(resarr,(-1, 4))
 # Write data to file using numpy.savetxt
     np.savetxt("tot.dat", res,
-           fmt='%3i % 15.4E % 15.4E % 15.4E')
+               fmt='%3i % 15.4E % 15.4E % 15.4E')
 
 # header="L\x/L\I2shifted\errorbar")
 if __name__ == "__main__":
