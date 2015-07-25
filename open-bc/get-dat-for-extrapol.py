30e265782aaa2414a61abd1c3ce6a0b5ca30b27d
Ipsita Mandal <imandal@perimeterinstitute.ca>
2015-07-24 22:14:14 -0400

    extract values for extrapolation

--- open-bc/get-dat-for-extrapol.py
+++ open-bc/get-dat-for-extrapol.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-# vim: set et ts=4 sw=4 tw=80:
-
+# vim: set et ts=4 sw=4 tw=100:
+# header="L \ x \ x/L \ 1/L \ I2shifted \ errorbar")
 
 import numpy as np
 
 def main():
     fdata = np.loadtxt("tot.dat")
-    val1  = fdata[:,1]
+    val1  = fdata[:,2]
     l=len(val1)
+    print(l)
     thres=1E-10
 
     for i in range(0,l-1):
         resarr = []
+        len_resarr = 1
         comval=val1[i] 
-        resarr.append([1/fdata[i,0], fdata[i,2], fdata[i,3]])
+        resarr.append([fdata[i,0], fdata[i,1],comval,
+         1/fdata[i,0], fdata[i,3], fdata[i,4]])
+         
         for j in range(i+1,l-1):
             if (np.abs(val1[j]-comval) < thres):
-                resarr.append([1/fdata[j,0], fdata[j,2], fdata[j,3]])
-        len_resarr = len(resarr[:])
+                len_resarr = len_resarr + 1 
+                resarr.append([fdata[j,0], fdata[j,1], comval, 
+                1/fdata[j,0], fdata[j,3], fdata[j,4]])
+                
+        #len_resarr = len(resarr[:,2])
+        #print(len_resarr)
         if (len_resarr > 1):
-             res = np.reshape(resarr,(-1, 3))
+             res = np.reshape(resarr,(-1, 6))
              np.savetxt("extra_{:0.3f}.dat".format(comval), res,
-                        fmt='% 15.4E % 15.4E % 15.4E')
+                        fmt='%3i %3i % 15.4E % 15.4E % 15.4E % 15.4E')
 
 if __name__ == "__main__":
     main()
