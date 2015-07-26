68892d6311148a57c8110810b319d7f0b5dc69c9
Ipsita Mandal <ipsita@linux-gfuc.site>
2015-07-25 21:26:47 -0400

    correction

--- open-bc/val_extr.py
+++ open-bc/val_extr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # vim: set et ts=4 sw=4 tw=100:
  
-# imported file name format: extra_x0.357140_D1.900000_T1.560014
+# imported file name format: extra_x0.357140_D1.900000_T1.560014.dat
 # having data in it as: L \ x \ x/L \ 1/L \ I2shifted \ errorbar")
 
 from glob import glob
@@ -21,6 +21,7 @@ fdict = {}
 for i in files:
     t = i.split('_')
     temp =t[3][1:]
+    print(temp)
     ratio = float(t[1] [1:])
 #print(ratio)
 # T=float(temp[0:4])
@@ -31,7 +32,8 @@ for i in files:
     fdict[(ratio,temp)] = i
 
     for temp in Temps:
-        for r in ratios:
+        for r in ratios: 
+            resarr = []
             fdata = np.loadtxt(fdict[(ratio,temp)])
             val1  = fdata[:,2]
             l=len(val1)
@@ -39,10 +41,15 @@ for i in files:
             yarray = np.array(list(fdata[i,4] for i in range(0,l)))
             popt, pcov = curve_fit(func, xarray, yarray)
             perr = np.sqrt(np.diag(pcov)) # this is error estimate array for fitting params
-            print(popt[0], perr[0])
-            #popt[0] = yintercept     popt[1] = slope
-            np.savetxt("extraval_D{0:.3f}_T{0:s}".format(D,temp), 
-                        '%1.8f %1.8f %1.8f \n' % ( r, popt[0],perr[0]))
+            print(r,popt[0], perr[0])
+
+            resarr.append( [r,popt[0], perr[0]] )
+
+            res = np.reshape(resarr, (-1,3))
+#popt[0] = yintercept     
+#popt[1] = slope
+            np.savetxt("cval_D%f_T%s"%( D,temp), 
+                        resarr,fmt=' % 15.4E % 15.4E % 15.4E')
 
 
         #for i in range(0,l-1):
