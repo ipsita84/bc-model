5be10b8d31994e039714efee899675477b2e9b2b
Ipsita Mandal <imandal@perimeterinstitute.ca>
2015-07-27 21:03:12 -0400

    code updated to scan over all (D,T)

--- data_analysis/chi2.py
+++ data_analysis/chi2.py
@@ -1,3 +1,8 @@
+# imported file name format: cval_D1.900_T1.540002.dat
+# having data in it as: x/L \ c from extrapolation \  errorbar"
+
+# output format: D_c T_c chi_sq-value
+
 import numpy as np
 import glob
 from scipy.optimize import leastsq
@@ -29,21 +34,31 @@ def err(X,Y,dY):
                 
 def main():
     fout = open('chi2.txt','w')
-    dede = linFit()
-    names = glob.glob("*1.900_*")
+    dede = linFit() 
+    names = glob.glob("cval_*")
     res = []
-    for fname in names:
+    Temps=[]
+    D=[]
+    for fname in names: 
+        t = fname.split('_')
+        temp =t[2][1:]
+        dval = float(t[1] [1:])
+        tt = temp.split('.')
+        temp1 = tt[0][0:]
+        temp2 = tt[1][0:]
+        T = float(temp1 + "." + temp2)
         d = np.loadtxt(fname)
         fitvals = [dede.val(i) for i in d[:,0]]
         fitvals = np.array(fitvals)
         chi2 = err(fitvals, d[:,1], d[:,2])
-        fout.write("%s Total chi^2 = %f\n" % (fname,chi2))
-        T = float(fname.split('T')[1].split('.dat')[0])
-        res.append((T,chi2))
+        #fout.write("%f %f %f\n" % (D, T ,chi2))
+        fout.write( "{0:.3f} {1:.3f} {2:.3f}\n".format(dval,T/2, chi2))
+        #T = float(fname.split('T')[1].split('.dat')[0])
+        res.append((dval,T,chi2))
     fout.close()
     res = np.array(res)
     fitf = lambda C,x,y: C[0]*(C[1] - x)**2 + C[2] - y
-    print (res[1:])
+    print (res[2:])
     x = leastsq(fitf,[1.,1.58,10.],(res[1:,0],res[1:,1]))
     print (x)
         
